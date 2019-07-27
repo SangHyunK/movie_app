47df7c0157eb1234e72a18f7efbd6b5921cc38de
KIMSANGHYUN <ksh9891@gmail.com>
2019-07-27 12:13:47 +0900

    Updates

---
+++ precache-manifest.6dc70ca84508357b58ed87dfd20df8c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9940b9348cfe6b105b1165edeb6e5a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e2c8a45a0cd16b0b4720",
+    "url": "/movie_app/static/css/main.d0bc95dc.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "e2c8a45a0cd16b0b4720",
+    "url": "/movie_app/static/js/main.37da9ff2.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
